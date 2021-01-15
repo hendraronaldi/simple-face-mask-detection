2fb40ad681dac9b39a8f162969641ee2b35d82b5
hendraronaldi <hendraronaldi@hotmail.com>
2021-01-15 17:17:39 +0700

    fix config files

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-port = $PORT\n\
-enableXsrfProtection = false\n\
-enableCORS = false\n\
 headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
 \n\
 " > ~/.streamlit/config.toml
